df02f2c732d19ad9f6316416bc9840fa41095dd3
flowglow <flowglow14@gmail.com>
2016-08-03 15:14:48 +0200

    Update Output_cube.py

--- Output_cube.py
+++ Output_cube.py
@@ -10,7 +10,7 @@ FileName = 'Cube_V0075_D12_ZZ_AB_SoftContact_FvsD.txt'
 with open(FileName,'w') as saveFile:	
 	for step in odb.steps.values():
 		for frame in step.frames:
-			RFTop = odb.rootAssembly.instances['MATRIX-1'].nodeSets['REFNODE']
+			RFTop = odb.rootAssembly.nodeSets['REFNODE']
 			RFTopDisplacement = frame.fieldOutputs['U'].getSubset(region=RFTop)
 			RFTopForce = frame.fieldOutputs['RF'].getSubset(region=RFTop)
 			Dy = RFTopDisplacement.values[0].data[1]
